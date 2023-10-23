b1183623b47798cdb33c6c904326bd85fe499be9
Tien Dung <dung14b@gmail.com>
2023-10-24 00:17:07 +0700

    init

---
+++ frontend/src/AppRoutes.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path='/'>
+        {/* <Route path='/' element='put your pages here' /> */}
+      </Route>
+    </Routes>
+  )
+}
+
+export default AppRouter;
