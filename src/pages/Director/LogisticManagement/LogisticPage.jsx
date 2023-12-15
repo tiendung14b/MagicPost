270f2fbfbf16626693cf69de429cdc24803aa24a
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2023-12-15 14:35:08 +0700

    add another page template

---
+++ src/pages/Director/LogisticManagement/LogisticPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./logistic_page.css";
+
+const LogisticPage = () => {
+  return (
+    <div className="manager">
+      <h1 className="page_title">Logistic Dashboard</h1>
+    </div>
+  );
+};
+
+export default LogisticPage;
---
+++ src/pages/Director/WarehouseManagement/WarehousePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./warehouse_page.css"
+
+const WarehousePage = () => {
+  return (
+    <div className="manager">
+      <h1 className="page_title">WareHouse Dashboard</h1>
+    </div>
+  );
+}
+
+export default WarehousePage
