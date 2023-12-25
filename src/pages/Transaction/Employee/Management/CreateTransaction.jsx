8c1f455cbab56af647ddb4f4854ccdc769caf884
Nguyễn Hoàng Anh Dũng <iovn1259@gmail.com>
2023-12-25 20:54:15 +0700

    set information value for transaction

--- src/pages/Transaction/Employee/Management/CreateTransaction.jsx
+++ src/pages/Transaction/Employee/Management/CreateTransaction.jsx
@@ -1,35 +1,139 @@
 import React from "react";
+import Loading from "../../../../ui/Loading/Loading";
+import Toast from "../../../../ui/Toast/Toast";
 import useLocation from "../../../../hooks/useLocation";
+import useTransactionEmployee from "../../../../hooks/useTransactionEmployee";
 import Button from "../../../../ui/Button/Button";
 import Input from "../../../../ui/Input/Input";
 import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "../../../CSS/Director.css";
 
-import arrow from "../../../../assets/arrow.svg";
-import fitler_icon from "../../../../assets/filter.svg";
-import default_avatar from "../../../../assets/default_avatar.png";
-
-const CreateTransaction = () => {
+const LocationInput = ({ id }) => {
   const { provinceData, districtData, getProvince, getDistrict } =
     useLocation(toast);
 
+  const [newTransaction, setNewTransaction] = useState({});
+
+  const handleChange = (name, value) => {
+    setNewTransaction({ ...newTransaction, [name]: value });
+  };
+
+  useEffect(() => {
+    getProvince();
+  }, []);
+
+  return (
+    <div className="choose__location">
+      <select
+        name="province"
+        id={`${id}_city_input`}
+        onChange={(e) => {
+          getDistrict(e.target.value);
+          let text = e.target.options[e.target.selectedIndex].text;
+          text = text.replace("Tỉnh ", "");
+          text = text.replace("Thành phố ", "");
+          console.log(text);
+        }}
+      >
+        <option value="" disabled>
+          Select Province
+        </option>
+        {provinceData?.map((province) => (
+          <option value={province.province_id}>{province.province_name}</option>
+        ))}
+      </select>
+      <select
+        name="district"
+        id={`${id}_district_input`}
+        onChange={(e) => {
+          console.log(e.target.options[e.target.selectedIndex].text);
+          let text = e.target.options[e.target.selectedIndex].text;
+          text = text.replace("Quận ", "");
+          text = text.replace("Huyện ", "");
+          console.log(text);
+        }}
+      >
+        <option value="" disabled>
+          Select District
+        </option>
+        {districtData?.map((district) => (
+          <option value={district.district_id}>{district.district_name}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+const CreateTransaction = () => {
   const [forms, setForms] = useState([{ id: 1 }]); // Initial form
+  const [packageCount, setPackageCount] = useState(1);
 
   const handleAddButtonClick = () => {
     const newForm = { id: forms.length + 1 };
     setForms([...forms, newForm]);
+    setPackageCount(packageCount + 1);
   };
 
   const handleRemoveForm = (formId) => {
     const updatedForms = forms.filter((form) => form.id !== formId);
     setForms(updatedForms);
+    setPackageCount(packageCount - 1);
   };
 
-  //useEffect
-  useEffect(() => {
-    getProvince();
-  }, []);
+  const getSenderInput = () => {
+    return {
+      name: window["sender_name_input"].value,
+      address: {
+        city: window["sender_city_input"].value,
+        district: window["sender_district_input"].value,
+        detail: window["sender_detail_input"].value,
+      },
+      phoneNumber: window["sender_phoneNumber_input"].value,
+      email: window["sender_email_input"].value,
+    };
+  };
+
+  const getReceiverInput = () => {
+    return {
+      name: window["receiver_name_input"].value,
+      address: {
+        city: window["receiver_city_input"].value,
+        district: window["receiver_district_input"].value,
+        detail: window["receiver_detail_input"].value,
+      },
+      phoneNumber: window["receiver_phoneNumber_input"].value,
+      email: window["receiver_email_input"].value,
+    };
+  };
+
+  const getPackageInput = () => {
+    // return {
+    //   name: window["package_name_input"].value,
+    //   description: window["package_description_input"].value,
+    //   type: window["package_type_input"].value,
+    //   weight: window["package_weight_input"].value,
+    //   quantity: window["package_quantity_input"].value,
+    // };
+    const packages = [];
+    for (let i = 0; i < packageCount; i++) {
+      packages.push({
+        name: window[`package_name_input_${i + 1}`].value,
+        description: window[`package_description_input_${i + 1}`].value,
+        type: window[`package_type_input_${i + 1}`].value,
+        weight: window[`package_weight_input_${i + 1}`].value,
+        quantity: window[`package_quantity_input_${i + 1}`].value,
+      });
+    }
+    return packages;
+  };
+
+  const getDeliveryInput = () => {
+    return {
+      transaction_type: window["package_transaction_type_input"].value,
+      prepaid: window["package_prepaid_input"].value,
+    };
+  };
 
   return (
     <div className="manager">
@@ -39,111 +143,63 @@ const CreateTransaction = () => {
           <div className="sender__input__title">
             <h3>1. Người gửi</h3>
           </div>
-          <Input className="form__input" placeholder="Tên người gửi"></Input>
-          <Input className="form__input" placeholder="Số điện thoại"></Input>
+          <Input
+            id="sender_name_input"
+            className="form__input"
+            placeholder="Tên người gửi"
+          />
+          <Input
+            id="sender_phoneNumber_input"
+            className="form__input"
+            placeholder="Số điện thoại"
+          />
           <div className="choose__location__title">
             <h4>Chọn địa chỉ</h4>
           </div>
 
-          <div className="choose__location">
-            <select
-              name="province"
-              id="province"
-              onChange={(e) => {
-                getDistrict(e.target.value);
-                let text = e.target.options[e.target.selectedIndex].text;
-                text = text.replace("Tỉnh ", "");
-                text = text.replace("Thành phố ", "");
-                console.log(text);
-              }}
-            >
-              <option value="" disabled>
-                Select Province
-              </option>
-              {provinceData?.map((province) => (
-                <option value={province.province_id}>
-                  {province.province_name}
-                </option>
-              ))}
-            </select>
-            <select
-              name="district"
-              id="district"
-              onChange={(e) => {
-                console.log(e.target.options[e.target.selectedIndex].text);
-                let text = e.target.options[e.target.selectedIndex].text;
-                text = text.replace("Quận ", "");
-                text = text.replace("Huyện ", "");
-                console.log(text);
-              }}
-            >
-              <option value="" disabled>
-                Select District
-              </option>
-              {districtData?.map((district) => (
-                <option value={district.district_id}>
-                  {district.district_name}
-                </option>
-              ))}
-            </select>
-          </div>
+          <LocationInput id={"sender"} />
 
-          <Input className="form__input" placeholder="Địa chỉ chi tiết"></Input>
-          <Input className="form__input" placeholder="Email"></Input>
+          <Input
+            id="sender_detail_input"
+            className="form__input"
+            placeholder="Địa chỉ chi tiết"
+          />
+          <Input
+            id="sender_email_input"
+            className="form__input"
+            placeholder="Email"
+          />
         </div>
         <div className="receiver__output">
           <div className="sender__input__title">
             <h3>2. Người nhận</h3>
           </div>
-          <Input className="form__input" placeholder="Tên người gửi"></Input>
-          <Input className="form__input" placeholder="Số điện thoại"></Input>
+          <Input
+            id="receiver_name_input"
+            className="form__input"
+            placeholder="Tên người gửi"
+          />
+          <Input
+            id="receiver_phoneNumber_phone"
+            className="form__input"
+            placeholder="Số điện thoại"
+          />
           <div className="choose__location__title">
             <h4>Chọn địa chỉ</h4>
           </div>
-          <div className="choose__location">
-            <select
-              name="province"
-              id="province"
-              onChange={(e) => {
-                getDistrict(e.target.value);
-                let text = e.target.options[e.target.selectedIndex].text;
-                text = text.replace("Tỉnh ", "");
-                text = text.replace("Thành phố ", "");
-                console.log(text);
-              }}
-            >
-              <option value="" disabled>
-                Select Province
-              </option>
-              {provinceData?.map((province) => (
-                <option value={province.province_id}>
-                  {province.province_name}
-                </option>
-              ))}
-            </select>
-            <select
-              name="district"
-              id="district"
-              onChange={(e) => {
-                console.log(e.target.options[e.target.selectedIndex].text);
-                let text = e.target.options[e.target.selectedIndex].text;
-                text = text.replace("Quận ", "");
-                text = text.replace("Huyện ", "");
-                console.log(text);
-              }}
-            >
-              <option value="" disabled>
-                Select District
-              </option>
-              {districtData?.map((district) => (
-                <option value={district.district_id}>
-                  {district.district_name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <Input className="form__input" placeholder="Địa chỉ chi tiết"></Input>
-          <Input className="form__input" placeholder="Email"></Input>
+
+          <LocationInput id={"receiver"} />
+
+          <Input
+            id="receiver_detail_input"
+            className="form__input"
+            placeholder="Địa chỉ chi tiết"
+          />
+          <Input
+            id="receiver_email_input"
+            className="form__input"
+            placeholder="Email"
+          />
         </div>
       </div>
       <div className="extra__form__input">
@@ -155,11 +211,16 @@ const CreateTransaction = () => {
             {forms.map((form) => (
               <div key={form.id} className="transaction__form">
                 <div>
-                  <Input className="form__input" placeholder="Tên đơn hàng" />
+                  <Input
+                    id={`package_name_input_${form.id}`}
+                    className="form__input"
+                    placeholder="Tên đơn hàng"
+                  />
                 </div>
 
                 <div>
                   <Input
+                    id={`package_description_input_${form.id}`}
                     className="form__input"
                     placeholder="Chi tiết đơn hàng"
                   />
@@ -167,9 +228,9 @@ const CreateTransaction = () => {
 
                 <div className="input__dropdown">
                   <h4>Loại đơn hàng</h4>
-                  <select name="" id="">
-                    <option value="">Đơn hàng thường</option>
-                    <option value="">Đơn hàng hộ</option>
+                  <select name="type" id={`package_type_input_${form.id}`}>
+                    <option value="PACKAGE">Hàng Hoá</option>
+                    <option value="DOCUMENT">Tài Liệu</option>
                   </select>
                 </div>
 
@@ -180,12 +241,14 @@ const CreateTransaction = () => {
                 <div className="transaction__value">
                   <div className="input__form">
                     <Input
+                      id={`package_weight_input_${form.id}`}
                       className="form__input__value"
                       placeholder="Cân nặng"
                     />
                   </div>
                   <div className="input__form">
                     <Input
+                      id={`package_quantity_input_${form.id}`}
                       className="form__input__value"
                       placeholder="Số lượng"
                     />
@@ -196,7 +259,10 @@ const CreateTransaction = () => {
                 <Button
                   className={"danger"}
                   text={"Xoá hàng hoá"}
-                  onClick={() => handleRemoveForm(form.id)}
+                  onClick={() => {
+                    handleRemoveForm(form.id);
+                    console.log(packageCount);
+                  }}
                 />
               </div>
             ))}
@@ -204,7 +270,10 @@ const CreateTransaction = () => {
             <Button
               text={"Thêm hàng hoá"}
               className={"action"}
-              onClick={handleAddButtonClick}
+              onClick={() => {
+                handleAddButtonClick();
+                console.log(packageCount);
+              }}
             />
           </div>
         </div>
@@ -216,26 +285,28 @@ const CreateTransaction = () => {
           <div className="transaction__delivery__container">
             <div className="input__dropdown">
               <h4>Chọn phương thức vận chuyển</h4>
-              <select name="" id="">
-                <option value="">COD</option>
-                <option value="">Chuyển Khoản</option>
+              <select id="package_transaction_type_input">
+                <option value="COD">COD</option>
+                <option value="Chuyển Khoản">Chuyển Khoản</option>
               </select>
             </div>
             <Input
+              id="package_prepaid_input"
               className="form__input"
               placeholder="Nhập phí trả trước"
-            ></Input>
+            />
           </div>
 
           <Button
             className={"action"}
             text={"Tạo đơn hàng"}
             onClick={() => {
-              toast.success("Tạo đơn hàng thành công");
+              console.log(getPackageInput());
             }}
           />
         </div>
       </div>
+      <ToastContainer className="toasify" />
     </div>
   );
 };
