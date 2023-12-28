eb3fd33917c4519e219147bb181beb9f3491f7dc
EttoNekko <100702053+EttoNekko@users.noreply.github.com>
2023-12-28 11:45:26 +0700

    thêm firebase host với folder build

--- src/api/clientAxios.js
+++ src/api/clientAxios.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 import Toast from '../ui/Toast/Toast'
 
 const clientAxios = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://magicpostapi.up.railway.app/api/',
   timeout: 10000,
   headers: {'X-Custom-Header': 'foobar'}
 });
 
 clientAxios.interceptors.request.use(
   (config) => {
-    config.baseURL = 'http://localhost:8000/api/';
+    config.baseURL = 'https://magicpostapi.up.railway.app/api/';
     config.timeout = 10000;
     config.headers = {
       'X-Custom-Header': 'foobar',
