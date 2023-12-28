f8921c340df40dbc162a0c7a976262cb324ec12b
EttoNekko <100702053+EttoNekko@users.noreply.github.com>
2023-12-28 11:22:28 +0700

    Revert "chỉnh base url"
    This reverts commit 27a03a61aca0b6c7b80727886110a525656c912d.

--- src/api/clientAxios.js
+++ src/api/clientAxios.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 import Toast from '../ui/Toast/Toast'
 
 const clientAxios = axios.create({
-  baseURL: 'https://magicpostapi.up.railway.app/api/',
+  baseURL: 'http://localhost:8000/api/',
   timeout: 10000,
   headers: {'X-Custom-Header': 'foobar'}
 });
 
 clientAxios.interceptors.request.use(
   (config) => {
-    config.baseURL = 'https://magicpostapi.up.railway.app/api/';
+    config.baseURL = 'http://localhost:8000/api/';
     config.timeout = 10000;
     config.headers = {
       'X-Custom-Header': 'foobar',
